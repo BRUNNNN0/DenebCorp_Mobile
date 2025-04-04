1806efdea2d0135f88b630db68275d26fb9e1c69
Bruno Gabriel <brunogaesperi@gmail.com>
2025-04-04 14:21:50 -0300

    Implementado o calendário para a seleção da data de nascimento em português.

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:i_pet/tela_cadastro.dart';
 import 'package:i_pet/tela_inicial.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:i_pet/utils/util.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
@@ -11,6 +12,14 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const[
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale(Util.kLanguageCode,Util.kCountryCode)
+      ],
       home: tela_cadastro(),
     );
   
