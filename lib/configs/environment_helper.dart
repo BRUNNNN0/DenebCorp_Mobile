8250ef6afe255c472f9f13408d84960114d18ae4
Bruno Gabriel <brunogaesperi@gmail.com>
2025-04-16 03:27:14 -0300

    implementação API cadastro

--- lib/configs/environment_helper.dart
+++ lib/configs/environment_helper.dart
@@ -1,16 +1,17 @@
 abstract interface class IEnvironmentHelper {
   String? get urlAuthentication;
-  String? get urlUserInformation;
+  String? get urlRegister;
 }
 
 final class EnvironmentHelper implements IEnvironmentHelper {
   const EnvironmentHelper();
 
-  String get _urlBase => 'http://192.168.56.1:8080';
+  String get _urlBase => 'http://10.0.0.111:8080';
 
   @override
   String? get urlAuthentication => '$_urlBase/auth/login';
 
   @override
-  String get urlUserInformation => '$_urlBase/user_information';
+  String get urlRegister => '$_urlBase/auth/register';
 }
+
--- lib/utils/util_text.dart
+++ lib/utils/util_text.dart
@@ -6,4 +6,17 @@ final class UtilText {
   static const String labelLoginFailure = 'Erro ao autenticar o usuário!';
   static const String labelLoginInvalidEmail = 'Informe um email válido!';
   static const String labelLoginInvalidPassword = 'Informe uma senha válida com no mímino 6 caracteres!';
+  
+  static const String labelRegisterTitle = 'Cadastrar';
+  static const String labelFirsname = 'Nome';
+  static const String labelLastname = 'Sobrenome';
+  static const String labelDTNascimento = 'Data de Nascimento';
+  static const String labelTelefone = 'Telefone';
+
+
+
+  static const String labelRegisterInvalidEmail = 'Email invalido!';
+  static const String labelRegisterInvalidPhone = 'Telefone invalido!';
+  static const String labelRegisterWeakPassword = 'Senha invalida!';
+  static const String labelRegisterFailure = 'Falha ao Registrar!';
 }
